b30f0053bd3b6c82bcb46ce3d6b8e7ef87868910
Hilal Ibrahim <62497696+hilalibrahim@users.noreply.github.com>
2024-08-02 17:19:05 +0530

    Update JobListings.vue

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -22,7 +22,7 @@ const state = reactive({
 
 onMounted(async () => {
   try {
-    const response = await axios.get('/api/jobs');
+    const response = await axios.get('https://json-server-1-wn3q.onrender.com/jobs');
     state.jobs = response.data;
   } catch (error) {
     console.error('Error fetching jobs', error);
